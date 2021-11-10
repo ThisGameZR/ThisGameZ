89b3537ec77f8acf38a943179fa03cf222c9b46e
ThisGameZR <timezazare@gmail.com>
2021-11-10 22:05:08 +0700

    About page 20%

--- components/About.js
+++ components/About.js
@@ -5,36 +5,42 @@ import { useRef } from "react";
 export default function About({ references }) {
     return (
         <div className="container-full about" ref={references.aboutRef}>
-            <div></div>
-            <div className="about__box">
-                <div className="about__box__header">
-                    <span>ABOUT ME</span>
-                </div>
-                <div className="about__box__content">
-                    <div className="about__box__content--left">
-                        <div className="image">
-                            <div></div>
-                        </div>
-                        <div className="description">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and
-                            scrambled it to make a type specimen book. It has
-                            survived not only five centuries, but also the leap
-                            into electronic typesetting, remaining essentially
-                            unchanged
+            <div className="about-content">
+                <div className="about-content-top">
+                    <div className="about-content-top-left">
+                        <div className="about-content-top-left-image">
+                            <Image
+                                src="/img/profile.jpg"
+                                layout="fill"
+                                quality="100"
+                            ></Image>
                         </div>
                     </div>
-                    <div className="about__box__content--right">
-                        <div className="header">SKILLS</div>
-                        <div className="content">
-                            <div></div>
+                    <div className="about-content-top-right">
+                        <div className="about-content-top-right-title">
+                            <h1>
+                                Hello! My name is Sahatham. I&apos;m currently
+                                studying at Chiang Mai University
+                            </h1>
+                            <h2>
+                                I&apos;m a full-stack web developer based in
+                                Thailand
+                            </h2>
+                        </div>
+                        <div className="about-content-top-right-text">
+                            <p>
+                                I majored in Computer Engineering, I expertise
+                                in Web-development. I&apos;ve always been
+                                someone who eager to learn new things. When I
+                                discovered web development in college, I&apos;ve
+                                been fascinated by it. So I decided to deepen my
+                                knowledge in web development.
+                            </p>
                         </div>
                     </div>
                 </div>
+                <div className="about-content-bottom"></div>
             </div>
-            <div></div>
         </div>
     );
 }
