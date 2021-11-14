a7340e176810b4631f2bf957711a426f9d25993b
ThisGameZR <timezazare@gmail.com>
2021-11-14 12:31:47 +0700

    About css

--- components/About.js
+++ components/About.js
@@ -21,22 +21,19 @@ export default function About({ references }) {
                             <h1>Hello! My name is Sahatham.</h1>
                             <h2>
                                 I&apos;m currently studying at Chiang Mai
-                                University
-                                <br />
-                                I&apos;m a full-stack web developer based in
-                                Thailand
+                                University.
+                                <br /> I&apos;m a full-stack web developer based
+                                in Thailand
                             </h2>
                         </div>
                         <div className="about-content-top-right-text">
                             <p>
                                 I majored in Computer Engineering, I expertise
-                                in Web-development.
-                                <br /> I&apos;ve always been someone who eager
-                                to learn new things.
-                                <br /> When I discovered web development in
-                                college, I&apos;ve been fascinated by it.
-                                <br /> So I decided to deepen my knowledge in
-                                web development.
+                                in Web-development. I&apos;ve always been
+                                someone who eager to learn new things. When I
+                                discovered web development in college, I&apos;ve
+                                been fascinated by it. So I decided to deepen my
+                                knowledge in web development.
                             </p>
                         </div>
                     </div>
